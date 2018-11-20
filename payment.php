1292f906928c2804fde44153e9357f09a8976f4b
Jacky Thewindmaker <jacky@thewindmaker.com>
2018-11-20 12:48:51 +0800

    1120

---
+++ payment.php
@@ -0,0 +1,18 @@
+<?php
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
+// Token is created using Checkout or Elements!
+// Get the payment token ID submitted by the form:
+$token = $_POST['stripeToken'];
+
+$charge = \Stripe\Charge::create([
+    'amount' => 999,
+    'currency' => 'usd',
+    'description' => 'Example charge',
+    'source' => $token,
+]);
+
+print_r($charge);
+?>
